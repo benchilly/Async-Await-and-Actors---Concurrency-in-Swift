b5655e6be4d4f8c2440d5e994c4cb5c0b5ede476
Jinpyo Hong <jphongs1005@gmail.com>
2026-05-15 21:09:51 +0900

    [Feat] AsyncSequence 프로젝트
    - Adapting Existing Callbacks or Handlers to AsyncSequence Using AsyncStream

---
+++ AsyncSequencePlayground.playground/Pages/Adapting-Async-Sequences.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class BitcoinPriceMonitor {
+    var price: Double = 0.0
+    var timer: Timer?
+    var priceHandler: (Double) -> Void = { _ in }
+    
+    @objc func getPrice() -> Void {
+        priceHandler(Double.random(in: 20000...40000))
+    }
+    
+    func startUpdating() -> Void {
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                      target: self,
+                      selector: #selector(getPrice),
+                      userInfo: nil,
+                      repeats: true)
+    }
+    
+    func stopUpdating() -> Void {
+        timer?.invalidate()
+    }
+}
+
+/*
+let bitcoinPriceMonitor: BitcoinPriceMonitor = BitcoinPriceMonitor()
+bitcoinPriceMonitor.priceHandler = { print($0) }
+bitcoinPriceMonitor.startUpdating()
+ */
+
+let bitcoinPriceStream: AsyncStream<Double> = AsyncStream(Double.self) { continuation in
+    let bitcoinPriceMonitor: BitcoinPriceMonitor = BitcoinPriceMonitor()
+    bitcoinPriceMonitor.priceHandler = { continuation.yield($0) }
+    
+    continuation.onTermination = { termination in
+        bitcoinPriceMonitor.stopUpdating()
+    }
+    
+    bitcoinPriceMonitor.startUpdating()
+}
+
+Task {
+    for await bitcoinPrice in bitcoinPriceStream {
+        print(bitcoinPrice)
+    }
+}
+
+//: [Next](@next)
